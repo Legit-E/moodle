c9b2e960a3f147ec97e4642ba9a06cb6fa03c2d5
Legit-E <ethanwhite2005@hotmail.com>
2026-07-31 02:03:21 +0100

    Create static profile screen

---
+++ lib/screens/profile.dart
@@ -0,0 +1,24 @@
+// lib/screens/profile.dart
+import 'package:flutter/material.dart';
+
+class ProfilePage extends StatelessWidget {
+  const ProfilePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Profile")),
+      body: const Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            CircleAvatar(radius: 40),
+            SizedBox(height: 10),
+            Text("Ethan White"),
+            Text("UP2301043"),
+          ],
+        ),
+      ),
+    );
+  }
+}
